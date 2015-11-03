3d92afddde03d8262bedfc6a62324b41847268e9
Henry Feild <hafeild@gmail.com>
2015-11-03 17:58:47 -0500

    Added a signup page and an integration test for it.
    Added password digests to the user fixtures.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    redirect_to :root
+    @user = User.new(user_params)
+    if @user.save
+        log_in @user
+        # @user.send_activation_email
+        flash[:success] = "Thanks for signing up!"
+        # flash[:success] = "Please check your email to activate your account."
+        redirect_to root_url
+    else
+        render 'new'
+    end
   end
 
   def destroy
@@ -17,5 +27,20 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,43 @@ class User < ActiveRecord::Base
   has_many :project_permissions
   has_many :created_projects, class_name: "Project", foreign_key: "created_by"
   
+  ## Validate the password -- must be present and between 8--50 characters.
+  has_secure_password
+  validates :password, presence: true, 
+            length: { minimum: 8, maximum: 50 }, allow_nil: true
 
+  ## Uses BCrypt to salt/encrypt a password.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  ## Creates a new random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  ## Adds a remember token to the database.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forgets a user (removes the remember token).
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+    ## Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    ## Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
