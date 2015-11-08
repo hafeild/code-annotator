14d7459eebae643778585be9c20b3c72ed839fef
Henry Feild <hafeild@gmail.com>
2015-11-08 08:44:54 -0500

    Added a bootstrap sidebar for project viewing.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($) {
 
-
   // Listen for a row to be clicked on. A td element must specifically be 
   // clicked (not a child element) to trigger the page load. For example, see
   // the project listing -- the td with the trash can does not cause the row's
