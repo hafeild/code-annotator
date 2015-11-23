d4afff15b60f845471304bcea2fa2fa690f24f76
Henry Feild <hafeild@gmail.com>
2015-11-22 22:11:11 -0500

    Added a controller and test to add new projects.

--- test/controllers/api/projects_controller_test.rb
+++ test/controllers/api/projects_controller_test.rb
@@ -9,14 +9,41 @@ class Api::ProjectsControllerTest < ActionController::TestCase
 
   test "should return success message on create" do
     log_in_as @user
-    @response = post :create, project: {}
-    assert JSON.parse(@response.body)['success']
+
+    assert_difference 'Project.count', 1, "Project not added." do 
+      assert_difference 'ProjectPermission.count', 1,"Permissions not added." do
+        response = post :create, project: {name: "my project"}
+        json_reponse = JSON.parse(response.body)
+        assert json_reponse['success'], 'Response unsuccessful.'
+        assert json_reponse['id'] == Project.last.id, "Project ids don't match."
+        permission = ProjectPermission.find_by(project_id: Project.last.id)
+        assert_not permission.nil?
+        # assert permissions.size == 1, "No permission created."
+        assert permission.user_id == @user.id, "User ids don't match."
+        assert permission.can_view, "User cannot view."
+        assert permission.can_author, "User cannot author."
+        assert permission.can_annotate, "User cannot annotate."
+      end
+    end
   end
+
+  test "should return error message on create with no name" do
+    log_in_as @user
+
+    assert_no_difference 'Project.count', "Project added." do 
+      assert_no_difference 'ProjectPermission.count',"Permissions added." do
+        response = post :create, project: {}
+        json_reponse = JSON.parse(response.body)
+        assert json_reponse['error'], 'Response successful.'
+      end
+    end
+  end
+
 
   test "should return list of projects on index when logged in" do
     log_in_as @user
-    @response = get :index
-    @projects = JSON.parse(@response.body)['projects']
+    response = get :index
+    @projects = JSON.parse(response.body)['projects']
     assert @projects 
 
     matches = 0;
@@ -32,25 +59,25 @@ class Api::ProjectsControllerTest < ActionController::TestCase
   end
 
   test "should return error message on index when not logged in" do
-    @response = get :index
-    assert JSON.parse(@response.body)['error'] == "You are not logged in."
+    response = get :index
+    assert JSON.parse(response.body)['error'] == "You are not logged in."
   end
 
   test "should return success message on show" do
     log_in_as @user
-    @response = get :show, id: 1
-    assert JSON.parse(@response.body)['success']
+    response = get :show, id: 1
+    assert JSON.parse(response.body)['success']
   end
 
   test "should return success message on update" do
     log_in_as @user
-    @response = patch :update, id: 1
-    assert JSON.parse(@response.body)['success']
+    response = patch :update, id: 1
+    assert JSON.parse(response.body)['success']
   end
 
   test "should return success message on delete" do
     log_in_as @user
-    @response = delete :destroy, id: 1
-    assert JSON.parse(@response.body)['success']
+    response = delete :destroy, id: 1
+    assert JSON.parse(response.body)['success']
   end
 end
