0ee3e14d8425490c8549f9d12ca1cc04261c8e24
Henry Feild <hafeild@gmail.com>
2015-12-01 15:58:18 -0500

    Added a fix for Taiga issue #155 (GitHub issue #21) -- endcoding issues on upload.
    This has only been tested in development. Requires a new library to be installed
    on the server (ICU).

--- test/controllers/files_controller_test.rb
+++ test/controllers/files_controller_test.rb
@@ -28,9 +28,15 @@ class FilesControllerTest < ActionController::TestCase
   test "should add new file if small enough and plain text" do
     log_in_as @user
     project = projects(:p1)
-    assert_difference 'ProjectFile.count', 1, "File not added." do 
+    assert_difference 'ProjectFile.count', 5, "Files not added." do 
       post :create, project_id: project.id, project_file: {
-        files: [fixture_file_upload("files/test.cpp", "text/plain")]
+        files: [
+          fixture_file_upload("files/test.cpp", "text/plain"),
+          fixture_file_upload("files/data-ascii.dat", "text/plain"),
+          fixture_file_upload("files/data-latin1.dat", "text/plain"),
+          fixture_file_upload("files/data-utf16.dat", "text/plain"),
+          fixture_file_upload("files/data-utf8.dat", "text/plain")
+        ]
       }
     end 
   end
