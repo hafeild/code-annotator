ef6027179c4533c47e7f0bc228b85b69387d4c7a
Henry Feild <hafeild@gmail.com>
2015-12-08 10:36:31 -0500

    Added tests for user controller edit and update.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,14 +10,90 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should accept update" do
-    patch :update, id: 1
-    assert_redirected_to :root
+  test "should accept update of name" do
+    log_in_as @user
+    patch :update, id: @user.id, user: {
+      name: "New Name", 
+      current_password: "password"
+    }
+    assert_redirected_to edit_user_path(@user), "Not redirected."
+    newUser = User.find(@user.id)
+    assert newUser.name == "New Name", "New name not saved."
+    assert newUser.email == @user.email, "Email changed."
+    assert newUser.password_digest == @user.password_digest
   end
 
-  test "should get edit" do
-    delete :edit, id: 1
+  test "should accept update of email" do
+    log_in_as @user
+    patch :update, id: @user.id, user: {
+      email: "new@email.com", 
+      current_password: "password"
+    }
+    assert_redirected_to edit_user_path(@user), "Not redirected."
+    newUser = User.find(@user.id)
+    assert newUser.name == @user.name, "Name changed."
+    assert newUser.email == "new@email.com", "Email not changed."
+    assert newUser.password_digest == @user.password_digest, "Password changed."
+  end
+
+
+  test "should accept update of password" do
+    log_in_as @user
+    patch :update, id: @user.id, user: {
+      password: "password2",
+      password_confirmation: "password2",
+      current_password: "password"
+    }
+    assert_redirected_to edit_user_path(@user), "Not redirected."
+    newUser = User.find(@user.id)
+    assert newUser.name == @user.name, "Name changed."
+    assert newUser.email == @user.email, "Email changed."
+    assert newUser.authenticate("password2"), "Password not saved."
+  end
+
+
+  test "a user should be able to change another users information." do
+    log_in_as users(:bar)
+    patch :update, id: @user.id, user: {
+      name: "New name",
+      email: "new@email.com",
+      password: "password2",
+      password_confirmation: "password2",
+      current_password: "password"
+    }
+    assert_redirected_to :root, "Not redirected."
+    newUser = User.find(@user.id)
+    assert newUser.name == @user.name, "Name changed."
+    assert newUser.email == @user.email, "Email changed."
+    assert newUser.password_digest == @user.password_digest, "Password changed."
+  end
+
+  test "a user shouldn't be able to change information if not logged in." do
+    patch :update, id: @user.id, user: {
+      name: "New name",
+      email: "new@email.com",
+      password: "password2",
+      password_confirmation: "password2",
+      current_password: "password"
+    }
+    assert_redirected_to :login, "Not redirected."
+    newUser = User.find(@user.id)
+    assert newUser.name == @user.name, "Name changed."
+    assert newUser.email == @user.email, "Email changed."
+    assert newUser.password_digest == @user.password_digest, "Password changed."
+  end
+
+
+  test "user should be able to edit their own settings" do
+    log_in_as @user
+    get :edit, id: @user.id
     assert_response :success
+  end
+
+  test "user shouldn't be able to edit another user" do
+    log_in_as @user
+    get :edit, id: users(:bar).id
+    assert_redirected_to :root
   end
 
   test "should accept deletion" do
