e4d89a86531d2eef8a14fc0c00f7802ef41fe50b
Henry Feild <hfeild@endicott.edu>
2016-02-19 13:18:27 -0500

    Added test for public links show controller.

--- test/controllers/api/public_links_controller_test.rb
+++ test/controllers/api/public_links_controller_test.rb
@@ -158,6 +158,37 @@ class Api::PublicLinksControllerTest < ActionController::TestCase
   end
 
   ## Test create controller.
+  test "create should generate a new link associated with the given project" do
+    log_in_as @user
+
+    assert_difference 'PublicLink.count', 1, "Link not created" do
+      response = post :create, project_id: @project.id, name: "A name"
+      link = JSON.parse(response.body)['public_link']
+      assert_not link.nil?, "Bad response: #{response.body}"
+
+      new_link = PublicLink.last
+
+      assert (new_link.name == "A name" and new_link.project_id == @project.id),
+        "Stored values are incorrect: #{new_link}"
+
+      assert (
+        link['id'] == new_link.id and 
+        link['project_id'] == @project.id and
+        link['name'] == "A name" and
+        link['link_uuid'] == new_link.link_uuid),
+        "Returned values are incorrect: #{response.body}"
+
+    end
+  end
+
+  test "create should only accept a name parameter" do
+    assert_no_difference 'PublicLink.count', "Link created" do
+      response = get :create, project_id: @project.id, 
+        public_link: {name: "hi", link_uuid: "hello"}
+      assert JSON.parse(response.body)['error'], 
+        "Error message not returned: #{response.body}"
+    end
+  end
 
 
   ## Test update controller.
