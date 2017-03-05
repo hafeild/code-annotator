bf96016e660012b3c8a2fdcc9c2748bf4d544840
Henry Feild <hank@feild.org>
2017-03-05 08:50:56 -0500

    Updated db seeds to include tags.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 userCount = 5
 projectCount = 50
 fileCount = 20
-tagCount = 10
+maxTagCount = 50
 
 # users = (1..5).to_a
 # projects = (1..50).to_a
@@ -52,10 +52,13 @@ userCount.times do |uid|
                activated: true)
 
   tags[users[-1].id] = []
-  tagCount.times do |i|
-    tag = Tag.create!(text: Faker::Lorem.words(num = 3, supplemental = false).join(" "),
-        user: users[-1])
-    tags[users[-1].id] << tag.id
+  rand(maxTagCount).times do |i|
+    begin
+      tag = Tag.create!(text: Faker::Lorem.words(
+        num = rand(2)+1, supplemental = false).join(" "), user: users[-1])
+        tags[users[-1].id] << tag.id
+    rescue
+    end
   end
 end
 
@@ -176,7 +179,7 @@ projectCount.times do
     if can_annotate or can_author or can_view
       ProjectPermission.create!(project_id: project.id, user_id: user.id,
         can_author: can_author, can_view: can_view, can_annotate: can_annotate )
-      tags[user.id].shuffle.take(2).each do |tagId|
+      tags[user.id].sample(rand(10)).each do |tagId|
         ProjectTag.create!(project_id: project.id, tag_id: tagId)
       end
     end
