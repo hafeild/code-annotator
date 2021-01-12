4a859f769ce53fadc214c0a5c1b03ffc08245d9b
Hank Feild <hank@feild.org>
2021-01-12 05:27:50 -0500

    Fixed issue in schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2017_03_01_033802) do
 
   create_table "alternative_codes", force: :cascade do |t|
     t.text "content"
